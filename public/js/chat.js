facbd01a59204924bd3bf97d4a62610afe7092d1
Timotei Mocan <mocantimoteidavid@gmail.com>
2022-09-27 17:07:03 +0200

    add nicer UX

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,14 +4,24 @@ socket.on("messageFromServer", (message) => {
     console.log(message);
 });
 
-document.getElementById("chatForm").addEventListener("submit", (e) => {
+const messageForm = document.getElementById("chatForm");
+const messageFormInput = document.getElementById("chatInput");
+const messageFormButton = document.getElementById("chatSendButton");
+const shareLocationButton = document.getElementById("shareLocationButton")
+
+messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    
+    messageFormButton.setAttribute("disabled", "disabled");
 
     const messageInput = e.target.elements.message;
     const submittedMessage = messageInput.value;
-    messageInput.value = "";
     
     socket.emit("messageFromClient", submittedMessage, (error) => {
+        messageFormButton.removeAttribute("disabled");
+        messageFormInput.value = "";
+        messageFormInput.focus();
+
         if (error) {
             return console.error(error);
         }
@@ -20,13 +30,16 @@ document.getElementById("chatForm").addEventListener("submit", (e) => {
     });
 })
 
-document.getElementById("shareLocation").addEventListener("click", () => {
+shareLocationButton.addEventListener("click", () => {
     if (!navigator.geolocation) {
         return alert("Your browser doesn't support geolocation.");
     }
 
+    shareLocationButton.setAttribute("disabled", "disabled");
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit("locationFromClient", { lat: position.coords.latitude, long: position.coords.longitude }, () => {
+            shareLocationButton.removeAttribute("disabled");
             console.log("Location sheared!");
         })
     })
